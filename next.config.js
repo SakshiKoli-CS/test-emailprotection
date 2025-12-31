ee264955b58ebff96daa310238991501e46d4bdd
SakshiKoli-CS <sakshi.koli@contentstack.com>
2025-12-31 20:25:39 +0530

    remove csp

--- next.config.js
+++ next.config.js
@@ -1,19 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  async headers() {
-    return [
-      {
-        source: '/:path*',
-        headers: [
-          {
-            key: 'Content-Security-Policy',
-            value: "script-src 'self' 'strict-dynamic' 'unsafe-inline' 'unsafe-eval' https://challenges.cloudflare.com https://ajax.cloudflare.com https:; object-src 'none';"
-          }
-        ]
-      }
-    ]
-  }
 }
 
 module.exports = nextConfig
